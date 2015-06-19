f58d7fb00dc8ba3994bed09cb084536e07aedb8c
Terri Regan <reganta@america.gov>
2015-06-19 16:21:00 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# america-page-protection
+American Page Protection
+
+Handles login for password protected pages within secure site.  Adds basic messaging.
---
+++ america-page-protection.php
@@ -0,0 +1,56 @@
+<?php
+/**********************************************************************************************************
+ Extends America Base Themes
+  
+ Plugin Name: 	  America Page Protection
+ Description:     This plugin handles authentication for password protected pages and adds basic error messaging.
+ 				  Needed due to enhanced security on production
+ Version:         1.0.0
+ Author:          Office of Design, Bureau of International Information Programs
+ License:         GPL-2.0+
+ Text Domain:     america
+ Domain Path:     /languages
+ 
+ ************************************************************************************************************/
+
+//* Prevent loading this file directly
+defined( 'ABSPATH' ) || exit;  
+
+add_action( 'init', 'iip_init' );
+add_filter( 'post_password_expires', 'iip_post_password_expires' );
+add_filter( 'the_password_form', 'iip_page_password_form' ); 
+
+
+function iip_init () {
+	load_plugin_textdomain ( 'america', FALSE, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+
+function iip_post_password_expires( $expires ) { // make cookie a session cookies
+ 	return 0;
+ }
+
+function iip_page_password_form() {
+	global $post;
+
+ 	$msg = '';
+ 	$url = plugin_dir_url( __FILE__ ) . 'iip-login.php?action=postpass';
+    $label = 'pwbox-'.( empty( $post->ID ) ? rand() : $post->ID );
+   
+    $form = '<form action="' . esc_url( $url ) . '" method="post"> <p>' . __( "To view this protected post, enter the password below:" ) . '</p>
+    <label for="' . $label . '">' . __( "Password:" ) . ' </label><input name="post_password" id="' . $label . '" type="password" size="20" maxlength="20" /><input type="submit" name="Submit" value="' . esc_attr__( "Submit" ) . '" />
+    </form>'; 
+
+   
+	if ( isset($_COOKIE['wp-postpass_' . COOKIEHASH]) && $_COOKIE['wp-postpass_' . COOKIEHASH] != $post->post_password) {
+	 	if( isset($_REQUEST['auth']) && $_REQUEST['auth'] == 1 ) {
+	 		 // Translate and escape.
+			$msg = esc_html__( 'Sorry, your password is wrong. Please try again.', 'america' );
+
+			// We have a cookie, but it doesn’t match the password.
+			$msg = "<p style='color:#C00;'>$msg</p>";
+		}
+	}
+ 	
+
+    return $msg . $form;
+}
